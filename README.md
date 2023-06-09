24a5e7bd98b05b65a5daf11ae54e7065a05aa551
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2023-06-09 17:45:38 +0200

    add readme, init app

---
+++ README.md
@@ -0,0 +1,1 @@
+# 3D Delevoper Portfolio
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        3D Developer Portfolio
+      </div>
+    </BrowserRouter>
   )
 }
 
